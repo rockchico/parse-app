b272b4c6c3f9e61f62796a9cd80f6bda74f6912f
Francisco Schwertner <rockchico@gmail.com>
2019-11-19 17:08:40 -0300

    testes com o Parse e React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
-import React from 'react';
+// https://www.back4app.com/docs/react/parse-react-sdk
+// https://docs.parseplatform.org/js/guide/#queries
+
+
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Parse from 'parse';
+
+
+Parse.serverURL = 'http://10.200.10.24:1337/parse';
+Parse.initialize("TESTE123", "123456");
+
+
+
+
+
 function App() {
+  
+  useEffect(() => {      
+    getData()
+  });
+  
+  
+  async function getData() {
+    const GameScore = Parse.Object.extend("GameScore");
+    const query = new Parse.Query(GameScore);
+    
+    //query.equalTo("playerName", "Francisco");
+    query.notEqualTo("playerName", "Francisco");
+
+
+    const results = await query.find();
+    console.log("Successfully retrieved " + results.length + " scores.");
+
+    // Do something with the returned Parse.Object values
+    for (let i = 0; i < results.length; i++) {
+      var object = results[i];
+      console.log(object.id + ' - ' + object.get('playerName'));
+    }
+  }
+  
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +57,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={getData}>        Show alert      </button>
       </header>
     </div>
   );
